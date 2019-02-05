4d0d089f2b8146bf98599f648690c49dcd7c1bd0
Serena Lertkantitham <lertkantitham@gmail.com>
2019-02-04 20:29:46 -0800

    initial server.js

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL } = require('./config');
+const { } = require('./models');
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan('common'));
+
+let server;
+
+function runServer(databaseUrl, port = {PORT}) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(
+      databaseUrl,
+      err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app
+          .listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve();
+          })
+          .on("error", err => {
+            mongoose.disconnect();
+            reject(err);
+          })
+      }
+    );
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log(`Closing server`);
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
